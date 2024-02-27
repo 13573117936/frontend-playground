1b4fa231be08457a9bc2c16beb0bddaffdcf751a
liuyunlong <>
2024-02-27 14:35:39 +0800

    route test

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/gemini" element={<App />}></Route>
+    </>
+  )
+);
