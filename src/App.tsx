59f8a45cde1e49c2cc1d5cb0e541bb63018aad60
liuyunlong <>
2024-02-27 16:13:44 +0800

    route test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { useNavigate } from "react-router-dom";
 
@@ -7,14 +5,6 @@ function App() {
   const nav = useNavigate();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => nav("/gemini")}>Nav to Gemini</button>
